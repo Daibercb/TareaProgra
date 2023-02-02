8c86610931e4a91934dafb83bc57f9760793bda7
Daiber Contreras B <daiberfranciscocontreras@gmail.com>
2023-02-01 23:34:03 -0600

    Cambio

--- AccesoDatos/ContactoDatos.cs
+++ AccesoDatos/ContactoDatos.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace AccesoDatos
 {
+
     public class ContactoDatos
     {
+        Entities con = new Entities();
+
+        public int RegistrarContacto(int id, string nom, string ape, string face,string insta, string tw)
+        {
+            return con.RegistrarContacto(id, nom, ape, face, insta, tw);
+        }
+
+
+
     }
 }
